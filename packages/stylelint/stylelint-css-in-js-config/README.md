749e12abbb081f42404ccc2dd9bb593f7d41b33f
Michal Jarnot <m.jarnot@yahoo.com>
2020-05-09 00:38:07 +0200

    feat: add css-in-js-config

---
+++ packages/stylelint/stylelint-css-in-js-config/README.md
@@ -0,0 +1,21 @@
+# StyleLint CSS-IN-JS Config
+
+[**stylelint**](https://github.com/stylelint/stylelint) modern linter that helps you avoid errors and enforce conventions in your styles.
+
+## Install
+
+```sh
+yarn add stylelint @linters/stylelint-css-in-js-config -D
+```
+
+## Usage
+
+Create a _.stylelintrc.js_ file with the following contents:
+
+```js
+module.exports = {
+  extends: [
+    '@linters/stylelint-css-in-js-config',
+  ],
+}
+```
---
+++ packages/stylelint/stylelint-css-in-js-config/index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    '@linters/stylelint-config',
+  ],
+  rules: {
+    //
+    // Turn off base rules
+    //
+
+    // Possible errors
+
+    'font-family-no-missing-generic-family-keyword': null,
+    'no-empty-source': null,
+    'string-no-newline': null,
+    'selector-pseudo-class-no-unknown': null,
+    'selector-pseudo-element-no-unknown': null,
+    'selector-type-no-unknown': null,
+    'no-descending-specificity': null,
+
+    // Limit language features
+
+    'selector-max-empty-lines': null,
+    'selector-no-vendor-prefix': null,
+
+    // Stylistic issues
+
+    'no-missing-end-of-source-newline': null,
+    'function-name-case': null,
+    'value-keyword-case': null,
+    'no-empty-first-line': null,
+  }
+}
