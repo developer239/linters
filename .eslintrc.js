8ad39d92423332f44089c288f81f20f18c3e9c05
michaljarnot <m.jarnot@yahoo.com>
2021-02-17 17:05:34 +0100

    feat: add code quality tools

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    '@linters/eslint-config-node',
+    'prettier',
+  ],
+  rules: {
+    'max-lines': 0
+  }
+}
---
+++ .stylelintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    '@linters/stylelint-css-in-js-config',
+  ],
+}
