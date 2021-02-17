408d68fdceee3ba6d930be769b2db09a682720cd
michaljarnot <m.jarnot@yahoo.com>
2021-02-17 13:38:09 +0100

    feat: remove default configuration

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    '@linters/eslint-config-node',
-    'prettier',
-  ],
-  rules: {
-    'max-lines': 'warn',
-  }
-}
--- commitlint.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  extends: [
-    '@linters/commitlint-config',
-  ],
-}
