126d91b6ea25eb590fd7aa157df32d6c5858f015
michaljarnot <m.jarnot@yahoo.com>
2023-02-03 18:10:34 +0100

    feat: improve css in js configuration for RN and WEB

--- packages/stylelint-css-in-js-config/index.js
+++ packages/stylelint-css-in-js-config/index.js
@@ -1,37 +1,10 @@
-const config = require('@linters/stylelint-config')
-
-delete config.rules['unit-disallowed-list']
-delete config.rules['media-feature-name-disallowed-list']
-
 module.exports = {
-  ...config,
-  processors: ['stylelint-processor-styled-components'],
+  extends: ['@linters/stylelint-config'],
+  customSyntax: '@stylelint/postcss-css-in-js',
   rules: {
-    ...config.rules,
-    //
-    // Turn off base rules
-    //
-
-    // Possible errors
-
-    'font-family-no-missing-generic-family-keyword': null,
-    'no-empty-source': null,
-    'string-no-newline': null,
-    'selector-pseudo-class-no-unknown': null,
-    'selector-pseudo-element-no-unknown': null,
-    'selector-type-no-unknown': null,
-    'no-descending-specificity': null,
-
-    // Limit language features
-
-    'selector-max-empty-lines': null,
-    'selector-no-vendor-prefix': null,
-
-    // Stylistic issues
-
-    'no-missing-end-of-source-newline': null,
-    'function-name-case': null,
-    'value-keyword-case': null,
+    // false positive
     'no-empty-first-line': null,
+
+    // TODO: add styled-components rules
   },
 }
--- packages/stylelint-css-in-js-react-native-config/index.js
+++ packages/stylelint-css-in-js-react-native-config/index.js
@@ -1,10 +1,9 @@
 module.exports = {
   customSyntax: '@stylelint/postcss-css-in-js',
-  extends: ['@linters/stylelint-config'],
+  extends: ['@linters/stylelint-css-in-js-config'],
   rules: {
     'selector-type-no-unknown': null,
     'unit-disallowed-list': null,
-    'no-empty-first-line': null,
 
     // TODO: add styled-components rules
   },
