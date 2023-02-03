b6e270320ee7753279a355d7713836d7ebb324b0
michaljarnot <m.jarnot@yahoo.com>
2023-02-03 17:42:05 +0100

    fix: stylelint-css-in-js-react-native-config
    - simplify and create minimal working version with eslint 14

--- packages/stylelint-css-in-js-react-native-config/index.js
+++ packages/stylelint-css-in-js-react-native-config/index.js
@@ -1,63 +1,11 @@
 module.exports = {
-  plugins: ['stylelint-react-native'],
-  extends: ['@linters/stylelint-css-in-js-config'],
+  customSyntax: '@stylelint/postcss-css-in-js',
+  extends: ['@linters/stylelint-config'],
   rules: {
-    //
-    // Disable base rules
-    //
+    'selector-type-no-unknown': null,
+    'unit-disallowed-list': null,
+    'no-empty-first-line': null,
 
-    //
-    // React Native Plugin
-    //
-
-    'property-no-unknown': [
-      true,
-      {
-        ignoreProperties: ['resize-mode', 'text-align-vertical'],
-      },
-    ],
-
-    //
-    // React Native Environment
-    //
-
-    'value-no-vendor-prefix': true,
-    'property-no-vendor-prefix': true,
-    'no-empty-source': null,
-    'no-missing-end-of-source-newline': null,
-    // stylelint-processor-styled-components creates
-    // a wrapping class selector for the component.
-    'selector-max-class': [
-      1,
-      {
-        severity: 'error',
-        message:
-          'class selectors are not supported when using styled-components with React Native.',
-      },
-    ],
-    'selector-max-universal': [
-      0,
-      {
-        severity: 'error',
-        message:
-          'universal selectors are not supported when using styled-components with React Native.',
-      },
-    ],
-    'selector-max-attribute': [
-      0,
-      {
-        severity: 'error',
-        message:
-          'attribute selectors are not supported when using styled-components with React Native.',
-      },
-    ],
-    'selector-max-id': [
-      0,
-      {
-        severity: 'error',
-        message:
-          'id selectors are not supported when using styled-components with React Native.',
-      },
-    ],
+    // TODO: add styled-components rules
   },
 }
